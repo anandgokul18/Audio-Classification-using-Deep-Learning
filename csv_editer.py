e783c4d56270f2c473062ba603c39b6d9caf9024
Anand Gokul <anandgokul@anandgokul.local>
2019-11-26 01:18:56 -0800

    Added CSV editor

--- csv_editer.py
+++ csv_editer.py
@@ -8,20 +8,24 @@ import pandas as pd
 #allfiles_dir = os.path.join(os.getcwd(),'sounds','train','train_sound')    # Directory to move images from
 
 df1 = pd.read_csv("/home/anandgokul18/audio/sounds/labels/train.csv")
+df2 = pd.DataFrame(columns=df1.columns)
 
 print(df1.head())
 import pdb
-pdb.set_trace()
 
 #target_ext = 'wav'                                  # File extension/type (used to filter input images)
 
 #_, _, file_names = next(os.walk(allfiles_dir))       # Get all image names
 
 for index, row in df1.iterrows():
-   if path.exists(row['ID']):
+   if not path.exists(row['ID']) and not path.isfile(str(row['ID'])+'.wav'):
+      #df1.drop(index , inplace=True)
       pass
    else:
-      df1.drop(index , inplace=True)
+      df2.append(row, ignore_index=True)
+
+
+df2.to_csv(file_name.csv, sep='\t')
 
 '''
 for name in file_names:
